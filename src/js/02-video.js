f615e55e62d2ff7108d292b294e0bf4acd7dc1b1
Julia Tushchenko <84866878+Julia-Tuschenko@users.noreply.github.com>
2021-10-30 23:05:28 +0300

    start 2

---
+++ src/js/02-video.js
@@ -0,0 +1,33 @@
+import Player from '@vimeo/player';
+// console.log(Player);
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const  onPlay = function(data) {
+};
+
+player.on('timeupdate', onPlay);
+
+player.off('timeupdate', onPlay);
+
+player.off('timeupdate');
+
+console.log(onPlay);
