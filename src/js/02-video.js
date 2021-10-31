711136113bd7f456e145a24fce57b37eaf3ba8e3
Julia Tushchenko <84866878+Julia-Tuschenko@users.noreply.github.com>
2021-10-31 14:36:48 +0200

    rwo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,28 +7,41 @@ import  throttle from "lodash.throttle";
 //     width: 640
 // });
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALPLAYER_KEY = 'videoplayer-current-time';
+
+
+
+function onPlay(data) {
+    const timePlayer = data.seconds;
+    localStorage.setItem(LOCALPLAYER_KEY, timePlayer);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+console.log(onPlay);
+
+
+function getTime() {
+    const saveTime = localStorage.getItem(LOCALPLAYER_KEY);
+
+    player
+      .setCurrentTime(saveTime)
+      .then()
+      .catch(function (error) {
+        return error.name;
+      });
+  }
+
+  player.on('play', throttle(getTime, 1000));
 // player.on('play', function() {
 //     console.log('played the video!');
 // });
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
 
-const  onPlay = function(data) {
-};
-
-player.on('timeupdate', onPlay);
-
-player.off('timeupdate', onPlay);
-
-player.off('timeupdate');
-
-console.log(onPlay);
